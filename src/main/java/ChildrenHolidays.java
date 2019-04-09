d29b6733a9941c0e440986f6d7aebc7575127a66
willu2 <bob@example.com>
2019-04-09 14:56:50 +0300

    v1

--- src/main/java/ChildrenHolidays.java
+++ src/main/java/ChildrenHolidays.java
@@ -1,5 +1,8 @@
 public class ChildrenHolidays {
     public static void main(String[] args) {
 
+        DataCreator dataCreator  = new DataCreator();
+        dataCreator.showAllData();
+
     }
 }
